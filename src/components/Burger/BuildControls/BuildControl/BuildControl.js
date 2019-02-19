a55d9827e418897254258fbaa45e9199eed1d2b2
Tzili hod <tzili.hod@usaepay.com>
2019-02-19 15:33:54 -0800

    initial commit

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './BuildControl.module.css';
+
+const buildControl = props => (
+    <div className={styles.buildControl}>
+        <div className={styles.label}>{props.label}</div>
+        <button 
+            className={styles.less} 
+            onClick={props.removed}
+            disabled={props.disabled}>Less</button>
+        <button 
+            className={styles.more} 
+            onClick={props.added}>More</button>
+    </div>
+);
+
+export default buildControl;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import styles from './Layout.module.css';
+
+const Layout = props => (
+    <Aux>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={styles.content}>
+            {props.children}
+        </main>
+    </Aux>
+)
+
+export default Layout;
